1734ed5828f8410b49569ee8a73a5e6b16e94f98
Zugudu <2cuco2@gmail.com>
2021-01-23 16:21:02 +0200

    fix admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
+from django.utils.translation import gettext, gettext_lazy as _
 
 # Register your models here.
-admin.site.register(models.User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+	fieldsets = (
+		(None, {'fields': ('username', 'password')}),
+		(_('In-game info'), {'fields': ('money',)}),
+		)
+	add_fieldsets = UserAdmin.add_fieldsets + (
+		(_('In-game info'), {'fields': ('money',)}),
+		)
+
+
+
+admin.site.register(models.User, CustomUserAdmin)
 admin.site.register(models.Artifact)
